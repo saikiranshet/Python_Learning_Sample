3b2d097a9d52ea132254231b60167ba285555818
saikiranshet <32921574+saikiranshet@users.noreply.github.com>
2024-07-12 17:57:39 +0000

    pattern8and9

---
+++ Patterns/Pattern8.py
@@ -0,0 +1,11 @@
+n = int(input("Enter the value of n"))
+for i in range(n):
+    for j in range(n-i-1):
+        print(" ",end="")
+    for j in range(2*i+1):
+        print("*",end="")
+    for j in range(n-i-1):
+        print(" ",end="")
+    print()
+
+        
